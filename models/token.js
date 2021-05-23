222b395f3138775ae5efd962ba78573542b2a801
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-23 20:42:48 +0500

    Add token model

---
+++ models/token.js
@@ -0,0 +1,23 @@
+const {Schema, model} = require('mongoose');
+
+const tokenSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        required: true
+    },
+    expires_at: {
+        type: Date, default: (Date.now + (10 * 60 * 1000))
+    },
+    created_at: {
+        type: Date, default: Date.now
+    },
+    updated_at: {
+        type: Date, default: Date.now
+    }
+});
+
+module.exports = model('Token', tokenSchema);
