7931af2e6640dfb79ee8209976e09708ded7556b
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-23 19:27:48 +0500

    Remove blank lines

--- service/listener.js
+++ service/listener.js
@@ -8,19 +8,18 @@ module.exports = function () {
 
 
         connection.createChannel(function (channelError, channel) {
-            if (channelError) throw channelError;
-
+            if (channelError) {
+                throw channelError;
+            }
             let queue = keys.QUEUE;
 
             // channel.assertQueue(queue, {durable: false});
             channel.prefetch(1);
             console.log('>>> awaiting RPC requests...');
             channel.consume(queue, function reply(msg) {
-
                 console.log(JSON.parse(msg.content.toString())) // output the request
 
                 let response = 12 // test response
-
                 channel.sendToQueue(msg.properties.replyTo,
                     Buffer.from(response.toString()), {correlationId: msg.properties.correlationId});
 
