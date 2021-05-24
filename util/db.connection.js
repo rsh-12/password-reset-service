d6a17b79574934396ef1db06ff726b40db6d40ed
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-24 08:02:30 +0500

    Add connection function

---
+++ util/db.connection.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const keys = require('../keys/index')
+
+module.exports = async function connection() {
+    try {
+        await mongoose.connect(
+            keys.MONGODB_URI, {
+                auth: {
+                    user: keys.MONGODB_USERNAME,
+                    password: keys.MONGODB_PASSWORD
+                },
+                // authSource: "admin",
+                useNewUrlParser: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true
+            }
+        )
+    } catch (e) {
+        console.log(e);
+    }
+};
