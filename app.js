cf84c4f38b41351900acf679a714d05bf3056cf7
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-24 08:02:57 +0500

    Import and start connection

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const logger = require('morgan');
 const listener = require('./service/listener')
 const indexRouter = require('./routes/index');
 const app = express();
+const start_connection = require('./util/db.connection');
+
 // const cookieParser = require('cookie-parser');
+// app.use(cookieParser());
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -32,5 +32,6 @@ app.use(function (err, req, res, next) {
 });
 
 listener();
+start_connection().catch(reason => console.log(reason));
 
 module.exports = app;
