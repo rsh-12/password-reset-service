234f17e9c67eea59bab47262491b784db801d5d1
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-24 22:39:44 +0500

    Remove queue, add email and token queues

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,6 +1,7 @@
 module.exports = {
     RABBIT_HOST: process.env.RABBIT_HOST,
-    QUEUE: process.env.QUEUE,
+    EMAIL_QUEUE: process.env.EMAIL_QUEUE,
+    TOKEN_QUEUE: process.env.TOKEN_QUEUE,
     MONGODB_URI: process.env.MONGODB_URI,
     MONGODB_USERNAME: process.env.MONGODB_USERNAME,
     MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
