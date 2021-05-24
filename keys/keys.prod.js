c1f82bdc20385900c3b5b975ef7e1ad3bfd73f53
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-24 08:03:28 +0500

    Add some env vars

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,4 +1,7 @@
 module.exports = {
     RABBIT_HOST: process.env.RABBIT_HOST,
     QUEUE: process.env.QUEUE,
+    MONGODB_URI: process.env.MONGODB_URI,
+    MONGODB_USERNAME: process.env.MONGODB_USERNAME,
+    MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
 }
