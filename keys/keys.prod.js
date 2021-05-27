a64b5ca98b6d34e3e302410c36215e0e07e62e10
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-27 22:43:24 +0500

    Add new key-value pairs

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,8 +1,14 @@
 module.exports = {
+    // app
+    BASE_URL: process.env.BASE_URL,
+    // message broker
     RABBIT_HOST: process.env.RABBIT_HOST,
     EMAIL_QUEUE: process.env.EMAIL_QUEUE,
     TOKEN_QUEUE: process.env.TOKEN_QUEUE,
+    // database
     MONGODB_URI: process.env.MONGODB_URI,
     MONGODB_USERNAME: process.env.MONGODB_USERNAME,
     MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
+    // email
+    MAIL_USER: process.env.MAIL_USER,
 }
