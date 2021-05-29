54a6deb94084204cac35fe5c1892047cecf5fd1e
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-05-29 22:08:42 +0500

    Update email.sender.js

--- service/email.sender.js
+++ service/email.sender.js
@@ -1,42 +1,15 @@
-const keys = require('../keys/index');
-const nodemailer = require('nodemailer');
 const bcrypt = require('bcryptjs');
 const Token = require('../models/token');
 const crypto = require('crypto');
+const reset = require('../util/email/template');
+const transporter = require('../util/email/transporter');
 
-
-function reset(email, token) {
-    return {
-        to: keys.MAIL_USER,
-        from: keys.MAIL_USER,
-        subject: 'Reset password',
-        html:
-            `
-            <h1>Hi ${email}!</h1>
-            <p>Some has requested a link to change your password, and you can do this through the link below.</p>
-            <a href="${keys.BASE_URL}/some/path?token=${token}">Change my password</a>
-            <br>
-            <p>If you didn't request this, please ignore this email.</p>
-            <p>Your password will not change until you access the link below and create a new one.</p>                    
-            `
-    }
-}
-
-const transporter = nodemailer.createTransport({
-    host: keys.MAIL_HOST,
-    port: 587,
-    secure: false,
-    auth: {
-        user: keys.MAIL_USER,
-        pass: keys.MAIL_PWD,
-    }
-});
-
-async function renewToken(username) {
+async function renewAndSendToken(username) {
     const query = Token.findOne({email: username});
+    let token = await bcrypt.hash(crypto.randomBytes(5).toString('hex'), 10);
     const update = {
         email: username,
-        token: await bcrypt.hash(crypto.randomBytes(5).toString('hex'), 10),
+        token: token,
         expires_at: Date.now() + 10 * 60 * 1000,
         updated_at: Date.now()
     };
@@ -44,10 +17,10 @@ async function renewToken(username) {
 
     Token.findOneAndUpdate(query, update, options, function (error, result) {
         if (error) return error;
-        console.log(result)
+        transporter.sendMail(reset(username, token))
     });
 }
 
 module.exports = async function (username) {
-    await renewToken(username);
+    await renewAndSendToken(username);
 }
