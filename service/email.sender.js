d90b2c36f35d3844baaaf3ea3c081454ff5baefc
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-01 15:55:22 +0500

    Uncomment mail sending

--- service/email.sender.js
+++ service/email.sender.js
@@ -22,7 +22,7 @@ async function renewAndSendToken(username) {
             console.error(error)
         }
         console.log(result.token)
-        // transporter.sendMail(reset(username, token))
+        transporter.sendMail(reset(username, token))
         success = true;
     });
 
