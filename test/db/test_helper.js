80bfa686e361f8f836c65e7ab280df4d80f75d30
jellyfish12jellyfish <grinvichforum12@mail.ru>
2021-06-03 22:53:10 +0500

    Update test_helper.js

--- test/db/test_helper.js
+++ test/db/test_helper.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 const keys = require('../../keys/index');
+const connection = require('../../util/db/db.connection')
 
-before(() => {
-    mongoose.connect(keys.MONGODB_TEST_URI, {
-        auth: {
-            user: keys.MONGODB_USERNAME,
-            password: keys.MONGODB_PASSWORD
-        },
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    }).catch(err => console.error(err))
-})
+if (keys.GITHUB) {
+    before(() => connection(keys.MONGODB_TEST_URI));
+} else {
+    before(() => {
+        mongoose.connect(keys.MONGODB_TEST_URI, {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        }).catch(err => console.error(err))
+    });
+}
+
 
 beforeEach((done) => {
     mongoose.connection.collections.tokens.drop(() => done())
